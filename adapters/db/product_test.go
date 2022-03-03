97ffe08e432e1d43bcfbe74bbceb78b8602bdedf
eskokado <eskokado@gmail.com>
2022-03-03 06:43:41 -0300

    feat: criando testes do method save

--- adapters/db/product_test.go
+++ adapters/db/product_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/eskokado/go-hexagonal/adapters/db"
+	"github.com/eskokado/go-hexagonal/application"
 	"github.com/stretchr/testify/require"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -66,3 +67,32 @@ func TestProductDb_Get(t *testing.T) {
 	require.Equal(t, 0.0, product.GetPrice())
 	require.Equal(t, "disabled", product.GetStatus())
 }
+
+func TestProductDb_Save(t *testing.T) {
+	// setUp()
+	Db, err := sql.Open("sqlite3", "../../db.sqlite")
+	if err != nil {
+		log.Fatal("########## Erro ao abrir sqlite3: " + err.Error())
+	}
+	defer Db.Close()
+	productDb := db.NewProductDb(Db)
+
+	product := application.NewProduct()
+	product.Name = "Product Test"
+	product.Price = 25
+
+	productResult, err := productDb.Save(product)
+	require.Nil(t, err)
+	require.Equal(t, product.Name, productResult.GetName())
+	require.Equal(t, product.Price, productResult.GetPrice())
+	require.Equal(t, product.Status, productResult.GetStatus())
+
+	product.Status = "enabled"
+
+	productResult, err = productDb.Save(product)
+	require.Nil(t, err)
+	require.Equal(t, product.Name, productResult.GetName())
+	require.Equal(t, product.Price, productResult.GetPrice())
+	require.Equal(t, product.Status, productResult.GetStatus())
+}
+
