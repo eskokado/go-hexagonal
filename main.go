24434d163a4355c4258f5821e891e7a7b01c0110
eskokado <eskokado@gmail.com>
2022-03-04 05:47:38 -0300

    feat: implement cli with cobra

--- main.go
+++ main.go
@@ -1,23 +1,11 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"database/sql"
-	"log"
-
-	"github.com/eskokado/go-hexagonal/adapters/db"
-	"github.com/eskokado/go-hexagonal/application"
-	_ "github.com/mattn/go-sqlite3"
-)
+import "github.com/eskokado/go-hexagonal/cmd"
 
 func main() {
-	Db, err := sql.Open("sqlite3", "./db.sqlite")
-	if err != nil {
-		log.Fatal("########## Erro ao abrir sqlite3: " + err.Error())
-	}
-	defer Db.Close()
-	productDbAdapter := db.NewProductDb(Db)
-	productService := application.NewProductService(productDbAdapter)
-	product, _ := productService.Create("Product example", 15.0)
-
-	productService.Enable(product)
+	cmd.Execute()
 }
